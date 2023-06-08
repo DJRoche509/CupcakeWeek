de4929a5c1fb3534e5b9b0fa45884744594ae041
DJRoche509 <davjroche@gmail.com>
2023-06-08 01:57:40 -0500

    Implement new object calls and created new variables

--- src/Main.java
+++ src/Main.java
@@ -8,16 +8,64 @@ public class Main {
         // Create an instance object of
 
         // Create arrayList cupcakeMenu to hold Cupcake objects
-        ArrayList <Cupcake> cupcakemenu = new ArrayList<Cupcake>();
+        ArrayList <Cupcake> cupcakeMenu = new ArrayList<Cupcake>();
 
         // Create instance objects of Cupcake, RedVelvet, Chocolate
         Cupcake cupcake = new Cupcake();
         RedVelvet redVelvet = new RedVelvet();
         Chocolate chocolate = new Chocolate();
 
+        System.out.println("We are in the middle of creating the cupcake menu. We currently have three cupcakes on the menu but we need to decide on pricing.");
+
         Scanner input = new Scanner(System.in);
 
-        System.out.println("We are deciding on the price for our standard cupcake. Here is the description");
+        System.out.println("We are deciding on the price for our standard cupcake. Here is the description:");
+
+        // Invoke cupcake.type()
+        cupcake.type();
+
+        System.out.println("How much would you like to charge for the cupcake? (Input a numerical number taken to 2 decimal places)");
+        // Get user's input for price to be charged
+        String priceText = input.nextLine();
+        // Convert priceText to the double variable, price
+        double price = Double.parseDouble(priceText);
+        // Invoke cupcake instance object to set the price by passing "price" as parameter
+        cupcake.setPrice(price) ;
+
+
+        /*   redVelvet   */
+       System.out.println("We are deciding on the price for our red velvet cupcake. Here is the description:");
+
+        // Invoke cupcake.type()
+        redVelvet.type();
+
+        System.out.println("How much would you like to charge for the red velvet cupcake? \n(Input a numerical number taken to 2 decimal places)");
+        // Get user's input for price to be charged
+        priceText = input.nextLine();
+        // Convert priceText to the double variable, price
+        price = Double.parseDouble(priceText);
+        // Set price for the redVelvet cupcake
+        redVelvet.setPrice(price) ;
+
+
+        /*   chocolate  */
+        System.out.println("We are deciding on the price for our chocolate cupcake. Here is the description:");
+
+        // Invoke cupcake.type()
+        chocolate.type();
+
+        System.out.println("How much would you like to charge for the chocolate cupcake? \n(Input a numerical number taken to 2 decimal places)");
+        // Get user's input for price to be charged
+       priceText = input.nextLine();
+        // Convert priceText to the double variable, price
+        price = Double.parseDouble(priceText);
+        // Set price for the chocolate cupcake
+        chocolate.setPrice(price) ;
+
+        // Add each cupcake object to the cupcakeMenu arrayList
+        cupcakeMenu.add(cupcake);
+        cupcakeMenu.add(redVelvet);
+        cupcakeMenu.add(chocolate);
     }
 }
 
